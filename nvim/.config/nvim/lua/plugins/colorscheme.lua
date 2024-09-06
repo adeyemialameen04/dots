44ee9c20bcb5b5d867731d91bea9f5feb871fe4f
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-09-06 23:44:04 +0100

    nvim: more colorschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -49,6 +49,25 @@ return {
     },
   },
   {
+    "rebelot/kanagawa.nvim",
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        TelescopeTitle = { fg = theme.ui.special, bold = true },
+        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+      }
+    end,
+  },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "scottmckendry/cyberdream.nvim", lazy = false },
+  { "neanias/everforest-nvim", version = false, lazy = false },
+  { "sainnhe/sonokai" },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
